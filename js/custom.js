0d3061a5e80935887e83c903d17a14480ace0ee7
Günsel Akkemik <GUNSELAKKEMIK@HOTMAIL.COM.TR>
2021-03-29 10:54:14 +0300

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,52 @@
+var body = $("#banner");
+var backgrounds = [
+  "url(../img/background/gorsel1.png)",
+  "url(../img/background/gorsel2.jpg)",
+  "url(../img/background/gorsel3.jpg)",
+  "url(../img/background/gorsel4.jpg)",
+  "url(../img/background/gorsel5.jpg)",
+  "url(../img/background/gorsel6.jpg)",
+  "url(../img/background/gorsel7.jpg)",
+  "url(../img/background/gorsel8.jpg)",
+];
+var current = 0;
+
+$(function () {
+  var body = $("#banner");
+
+  function nextBackground() {
+    body.css({
+      "background-image":
+        "linear-gradient(to left, rgba(0, 0, 0, .6), transparent 27%), linear-gradient(to bottom, rgba(0, 0, 0, .6), transparent 27%), linear-gradient(to right, rgba(0, 0, 0, .6), transparent 27%)," +
+        backgrounds[(current = ++current % backgrounds.length)],
+    });
+
+    setTimeout(nextBackground, 20000);
+  }
+  setTimeout(nextBackground, 20000);
+  body.css({
+    "background-image":
+      "linear-gradient(to left, rgba(0, 0, 0, .6), transparent 27%), linear-gradient(to bottom, rgba(0, 0, 0, .6), transparent 27%), linear-gradient(to right, rgba(0, 0, 0, .6), transparent 27%)," +
+      backgrounds[0],
+  });
+});
+
+$(function () {
+  $("#banner").click(function () {
+    nextBackground();
+  });
+});
+
+function nextBackground() {
+  body.css({
+    "background-image":
+      "linear-gradient(to left, rgba(0, 0, 0, .6), transparent 27%), linear-gradient(to bottom, rgba(0, 0, 0, .6), transparent 27%), linear-gradient(to right, rgba(0, 0, 0, .6), transparent 27%)," +
+      backgrounds[(current = ++current % backgrounds.length)],
+  });
+}
+
+$(document).ready(function () {
+  $(".switchInner").on("click", function (e) {
+    $(".switchInner").css("bottom", "25px");
+  });
+});
