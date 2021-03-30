a7abc1610886e33663121d321843097f1bafcd16
Günsel Akkemik <GUNSELAKKEMIK@HOTMAIL.COM.TR>
2021-03-30 22:44:51 +0300

    Better slider and more responsive

--- js/custom.js
+++ js/custom.js
@@ -82,7 +82,7 @@ $(document).ready(function () {
   });
 });
 
-$(document).ready(function(){       
+$(function() {     
   var scroll_start = 0;
   var startchange = $('#banner');
   var offset = startchange.offset();
@@ -111,5 +111,169 @@ $(document).ready(function(){
       }
   });
    }
+
+$("#myCarousel").on("slide.bs.carousel", function(e) {
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 3;
+  var totalItems = $(".carousel-item").length;
+
+  if (idx >= totalItems - (itemsPerSlide - 1)) {
+    var it = itemsPerSlide - (totalItems - idx);
+    for (var i = 0; i < it; i++) {
+      // append slides to end
+      if (e.direction == "left") {
+        $(".carousel-item")
+          .eq(i)
+          .appendTo(".carousel-inner");
+      } else {
+        $(".carousel-item")
+          .eq(0)
+          .appendTo($(this).find(".carousel-inner"));
+      }
+    }
+  }
 });
 
+});
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  console.log("123");
+  new Splide('#splide', {
+    type: 'loop',
+    perPage: 3,
+    focus: 'center',
+    autoplay: true,
+    interval: 8000,
+    flickMaxPages: 3,
+    updateOnMove: true,
+    pagination: true,
+    autoWidth: true,
+    trimSpace: false,
+    gap: 205,
+    padding: {
+      left : '5rem',
+      right: '8rem',
+    },
+    throttle: 100,
+    breakpoints: {
+      1440: {
+        perPage: 2,
+        padding: {
+          left : '8rem',
+          right: '0',
+        },
+        gap: 300,
+        focus : 'center',
+      },
+      1300: {
+        perPage: 2,
+        padding: {
+          left : '9rem',
+          right: '0',
+        },
+        gap: 200,
+        focus : 'center',
+      },
+      1300: {
+        perPage: 2,
+        padding: {
+          left : '7rem',
+          right: '0',
+        },
+        gap: 150,
+        focus : 'center',
+      },
+      1100: {
+        perPage: 2,
+        padding: {
+          left : '6%',
+          right: '0',
+        },
+        gap: 100,
+        focus : 'center',
+      },
+      1000: {
+        perPage: 2,
+        padding: {
+          left : '4%',
+          right: '0',
+        },
+        gap: 60,
+        focus : 'center',
+      },
+      920: {
+        perPage: 2,
+        padding: {
+          left : '4%',
+          right: '0',
+        },
+        gap: 40,
+        focus : 'center',
+      },
+      900: {
+        perPage: 1,
+        padding: {
+          left : '25%',
+          right: '0',
+        },
+        gap: 300,
+        focus : 'center',
+      },
+      750: {
+        perPage: 1,
+        padding: {
+          left : '15%',
+          right: '0',
+        },
+        gap: 300,
+        focus : 'center',
+      },
+      580: {
+        perPage: 1,
+        padding: {
+          left : '14%',
+          right: '0',
+        },
+        gap: 300,
+        focus : 'center',
+      },
+      480: {
+        perPage: 1,
+        padding: {
+          left : '8.5%',
+          right: '0',
+        },
+        gap: 300,
+        focus : 'center',
+      },
+      450: {
+        perPage: 1,
+        padding: {
+          left : '0',
+          right: '0',
+        },
+        gap: 300,
+        focus : 'center',
+      },
+      400: {
+        perPage: 1,
+        padding: {
+          left : '2.5%',
+          right: '0',
+        },
+        gap: 300,
+        focus : 'center',
+      },
+      330: {
+        perPage: 1,
+        padding: {
+          left : '1%',
+          right: '0',
+        },
+        gap: 300,
+        focus : 'center',
+      },
+    }
+  }).mount();
+});
